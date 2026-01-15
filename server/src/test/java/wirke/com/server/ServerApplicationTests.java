9c2bfc07226a2c044d68f1b936108f4c6be0a4e6
wirke <wiriyevich@gmail.com>
2026-01-15 21:48:43 +0100

    created spring web server

---
+++ server/src/test/java/wirke/com/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wirke.com.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
