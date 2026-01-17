ccc95b2828b420cc693b9b3b0f884876699601b3
wirke <wiriyevich@gmail.com>
2026-01-17 14:54:25 +0100

    Migrated simple app to react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,46 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Podaci</h1>
+      <div class="card">
+          <div>Temperatura</div>
+          <div id="temp" class="value">--</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="card">
+          <div>Vlažnost</div>
+          <div id="vlaga" class="value">--</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="card">
+          <div>Doba dana</div>
+          <div id="status" class="value">--</div>
+      </div>
     </>
   )
 }
+
+const espIP = "http://192.168.0.101/data";
+
+async function updateData() {
+    try {
+        const response = await fetch(espIP);
+        const data = await response.json();
+        
+        document.getElementById('temp').innerText = data.temp + " °C";
+        document.getElementById('vlaga').innerText = data.vlaga + " %";
+        const statusElement = document.getElementById('status');
+
+if (data.status === "NOC") {
+	statusElement.innerText = "Noć";
+} else {
+	statusElement.innerText = "Dan";
+}
+    } catch (err) {
+        console.log("Greška pri osvežavanju:", err);
+    }
+}
+
+setInterval(updateData, 2000);
 
 export default App
